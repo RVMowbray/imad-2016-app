4a3a290f3f12e95cdaa0dab879eb441b9b4b5d7b
RVMowbray <mowbrayrv@gmail.com>
2016-09-28 19:33:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,9 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 app.get('/art1', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'm.html'));
 });
+
+app.get('/art1', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'm.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
